6838319b647cdd331f80c9142eeaeba8b3d7efcd
khamal45 <khamal45@proton.me>
2026-05-09 09:37:54 +0700

    add feat laporan dempul

---
+++ app/Exports/LaporanDempulExport.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Exports;
+
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
+use PhpOffice\PhpSpreadsheet\Style\Alignment;
+use PhpOffice\PhpSpreadsheet\Style\Border;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+
+class LaporanDempulExport implements FromCollection, WithHeadings, WithStyles, WithEvents
+{
+    protected $data;
+    protected $tanggal;
+
+    public function __construct($data, $tanggal)
+    {
+        $this->data = $data;
+        $this->tanggal = $tanggal;
+    }
+
+    public function collection()
+    {
+        $rows = collect();
+        $detailProduksi = $this->data['detail'] ?? [];
+        $summaryProduksi = $this->data['summary'] ?? [];
+
+        $max = max(count($detailProduksi), count($summaryProduksi));
+
+        for ($i = 0; $i < $max; $i++) {
+            $row = [];
+            
+            // Left side (Detail)
+            if ($i < count($detailProduksi)) {
+                $d = $detailProduksi[$i];
+                $row['d_tgl'] = $d['tanggal'];
+                $row['d_p'] = $d['p'];
+                $row['d_l'] = $d['l'];
+                $row['d_t'] = $d['t'];
+                $row['d_jenis'] = $d['jenis'];
+                $row['d_grade'] = $d['grade'];
+                $row['d_byk'] = $d['byk'];
+                $row['d_m3'] = ''; 
+            } else {
+                $row['d_tgl'] = $row['d_p'] = $row['d_l'] = $row['d_t'] = $row['d_jenis'] = $row['d_grade'] = $row['d_byk'] = $row['d_m3'] = '';
+            }
+
+            $row['spacer'] = ''; 
+
+            // Right side (Summary)
+            if ($i < count($summaryProduksi)) {
+                $s = $summaryProduksi[$i];
+                $row['s_tgl'] = $s['tanggal'];
+                $row['s_ttl_pkj'] = $s['ttl_pkj'];
+                $row['s_harga'] = ''; 
+                $row['s_total_m3'] = ''; 
+                $row['s_ongkos_m3'] = ''; 
+                $row['s_ongkos_lb'] = ''; 
+            } else {
+                $row['s_tgl'] = $row['s_ttl_pkj'] = $row['s_harga'] = $row['s_total_m3'] = $row['s_ongkos_m3'] = $row['s_ongkos_lb'] = '';
+            }
+
+            $rows->push($row);
+        }
+
+        return $rows;
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Tanggal', 'p', 'l', 't', 'jenis', 'grade', 'byk', 'm3',
+            '', 
+            'Tanggal', 'TTL PKJ', 'HARGA', 'Total m3', 'ONGKOS PER M3', 'ONGKOS PER LB'
+        ];
+    }
+
+    public function styles(Worksheet $sheet)
+    {
+        $sheet->getStyle('A1:O1')->getFont()->setBold(true);
+        $sheet->getStyle('A1:O1')->getAlignment()->setHorizontal(Alignment::HORIZONTAL_CENTER);
+        return [];
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+                $sheet = $event->sheet->getDelegate();
+                $lastRow = $sheet->getHighestRow();
+
+                $sheet->getStyle("A1:H" . $lastRow)->applyFromArray([
+                    'borders' => ['allBorders' => ['borderStyle' => Border::BORDER_THIN]],
+                ]);
+                $sheet->getStyle("J1:O" . $lastRow)->applyFromArray([
+                    'borders' => ['allBorders' => ['borderStyle' => Border::BORDER_THIN]],
+                ]);
+
+                $sheet->getStyle("I1:I" . $lastRow)->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setARGB('000000');
+                $sheet->getStyle('N1:O1')->getFill()->setFillType(Fill::FILL_SOLID)->getStartColor()->setARGB('FFFF00');
+
+                $sheet->getColumnDimension('A')->setWidth(15);
+                $sheet->getColumnDimension('B')->setWidth(8);
+                $sheet->getColumnDimension('C')->setWidth(8);
+                $sheet->getColumnDimension('D')->setWidth(8);
+                $sheet->getColumnDimension('E')->setWidth(15);
+                $sheet->getColumnDimension('F')->setWidth(10);
+                $sheet->getColumnDimension('G')->setWidth(8);
+                $sheet->getColumnDimension('H')->setWidth(10);
+                $sheet->getColumnDimension('I')->setWidth(3); 
+                $sheet->getColumnDimension('J')->setWidth(15);
+                $sheet->getColumnDimension('K')->setWidth(10);
+                $sheet->getColumnDimension('L')->setWidth(15);
+                $sheet->getColumnDimension('M')->setWidth(15);
+                $sheet->getColumnDimension('N')->setWidth(18);
+                $sheet->getColumnDimension('O')->setWidth(18);
+            },
+        ];
+    }
+}
